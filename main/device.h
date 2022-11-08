ee594859bd310f9582c52cb9c4d07d82755a04fd
Antonios Karagiannis <antokarag@gmail.com>
2022-11-08 16:07:54 -0700

    WIP: basic sensors test

---
+++ main/device.h
@@ -0,0 +1,13 @@
+/**
+ * @file device.h
+ * @author Antonios Karagiannis (antokarag@gmail.com)
+ * @brief definitions for the Device that will be added to the Home Assistant
+ * @version 0.1
+ * @date 2022-11-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#define DEVICE_ID "waterMonitorTest"
+#define DEVICE_NAME "Water Monitor Test"
+#define FIRMWARE_VERSION "1.0.0"
